d598e1df35a1be6aeb134aa024e2ab76a8ce792b
Min Zhang <min@near.org>
2022-12-14 17:51:01 -0500

    version 2: just one big contract

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,75 +1,174 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::serde::Serialize;
-use near_sdk::{env, AccountId, Balance, near_bindgen, Promise, PanicOnDefault};
-use near_sdk::collections::{Vector};
+use near_sdk::serde::{Serialize, Serializer};
+use near_sdk::{env, AccountId, Balance, near_bindgen, Promise, PanicOnDefault, serde_json};
+use near_sdk::collections::{LookupMap, UnorderedSet, Vector};
+use serde::ser::SerializeStruct;
 
 const NEAR: Balance = 1_000_000_000_000_000_000_000_000;
-const MIN_BALANCE: Balance = NEAR;
 
-#[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-#[derive(PanicOnDefault)]
-struct EventContract {
+#[derive(Serialize)]
+pub struct EventSpec {
   // Max people that can join this event
-  max_num: u32,
+  max_num: u64,
   // Min people need to join this event to make it happen
-  min_num: u32,
+  min_num: u64,
+  // In yoctoNear
+  price: Balance,
+  // if the number of participants do not meet the target by this time, all funds will be returned
+  // Number of non-leap-nanoseconds since January 1, 1970 0:00:00 UTC.
+  deadline: u64,
+  // If the event raises money successfully, where the money will go to. Usually this will
+  // be set as the event owner
+  benefiary: AccountId,
+}
+
+type EventId = String;
+
+#[derive(BorshDeserialize, BorshSerialize)]
+struct Event {
+  // Max people that can join this event
+  max_num: u64,
+  // Min people need to join this event to make it happen
+  min_num: u64,
   cur_participants: Vector<AccountId>,
   // In yoctoNear
   price: Balance,
   // if the number of participants do not meet the target by this time, all funds will be returned
   // Number of non-leap-nanoseconds since January 1, 1970 0:00:00 UTC.
   deadline: u64,
+  // If the event raises money successfully, where the money will go to. Usually this will
+  // be set as the event owner
+  beneficiary: AccountId,
 }
 
-#[near_bindgen]
-impl EventContract {
-  #[init]
-  #[private] // Public - but only callable by env::current_account_id()
-  pub fn init(max_num: u32, min_num: u32, price: Balance, deadline: u64) -> Self {
-    Self {
-      max_num,
-      min_num,
-      cur_participants: Vector::new(b"m"),
-      price,
-      deadline,
-    }
+impl Serialize for Event {
+  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+    let mut state = serializer.serialize_struct("Event", 5)?;
+    state.serialize_field("max_num", &self.max_num)?;
+    state.serialize_field("min_num", &self.min_num)?;
+    state.serialize_field("cur_participants", &self.cur_participants.to_vec())?;
+    state.serialize_field("price", &self.price)?;
+    state.serialize_field("deadline", &self.deadline)?;
+    state.end()
+  }
+}
+
+#[derive(Debug)]
+struct Error(String);
+
+impl Event {
+  fn new(spec: EventSpec, event_id: &String) -> Self {
+      Self {
+        max_num: spec.max_num,
+        min_num: spec.min_num,
+        cur_participants: Vector::new(event_id.try_to_vec().unwrap()),
+        price: spec.price,
+        deadline: spec.deadline,
+        beneficiary: spec.benefiary,
+      }
   }
 
-  #[payable]
-  pub fn join(&mut self, account_id: AccountId) {
-    if env::attached_deposit() != self.price {
-      // TODO: not sure what's the right way to error here
-      env::panic_str(&"Participant did not attach the right balance")
+  fn join(&mut self, deposit: Balance, account_id: &AccountId) -> Result<(), Error> {
+    if deposit != self.price {
+      return Err(Error(format!("Participant did not attach the right balance, price {} attached: {}", self.price, deposit)));
     }
 
     for acc in self.cur_participants.iter() {
-      if acc == account_id {
-        env::panic_str(&format!("{:?} has already joined", account_id));
+      if &acc == account_id {
+        return Err(Error(format!("{:?} has already joined", account_id)));
       }
     }
-    self.cur_participants.push(&account_id)
-  }
 
-  pub fn view_participants(&self) -> Vec<AccountId> {
-    self.cur_participants.to_vec()
-  }
-
-  pub fn price(&self) -> Balance {
-    self.price
-  }
-
-  pub fn check_deadline(&mut self) {
-    if env::block_timestamp() >= self.deadline {
-        // TODO: not complete now just experimenting
-      let refund = (env::account_balance() - MIN_BALANCE) / self.cur_participants.len() as u128;
-      self.cur_participants.iter().fold(None, |p: Option<Promise>, acc| {
-        if let Some(p) = p {
-          Some(p.then(Promise::new(acc).transfer(refund)))
-        } else {
-          Some(Promise::new(acc).transfer(refund))
-        }
-      });
+    if self.cur_participants.len() >= self.max_num {
+      return Err(Error(format!("Event is already full")));
     }
+
+    self.cur_participants.push(account_id);
+
+    Ok(())
+  }
+}
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+struct Contract {
+  event_ids: Vector<EventId>,
+  events: LookupMap<EventId, Event>,
+  events_by_owner: LookupMap<AccountId, Vector<EventId>>,
+  events_by_participants: LookupMap<AccountId, Vector<EventId>>,
+}
+
+impl Default for Contract {
+  fn default() -> Self {
+      Self {
+        event_ids: Vector::new(b"a"),
+        events: LookupMap::new(b"b"),
+        events_by_owner: LookupMap::new(b"c"),
+        events_by_participants: LookupMap::new(b"d"),
+      }
+  }
+}
+
+#[near_bindgen]
+impl Contract {
+  pub fn start_event(&mut self, account_id: AccountId, event_spec: EventSpec) -> EventId {
+    env::setup_panic_hook();
+    let event_id: EventId = format!("event_{}", self.event_ids.len());
+    let event = Event::new(event_spec, &event_id);
+
+    self.events.insert(&event_id, &event);
+    let mut events = self.events_by_owner.get(&account_id).unwrap_or_else(|| Vector::new(format!("o#{}", account_id).as_bytes()));
+    events.push(&event_id);
+    self.events_by_owner.insert(&account_id, &events);
+    self.event_ids.push(&event_id);
+    self.event_ids.push(&event_id);
+    event_id
+  }
+
+  #[payable]
+  pub fn join(&mut self, event_id: EventId, account_id: AccountId)  {
+    env::setup_panic_hook();
+    let mut event = self.get_event(&event_id).expect("Event does not exist");
+    event.join(env::attached_deposit(), &account_id).unwrap();
+    self.events.insert(&event_id, &event);
+
+    let mut events = self.events_by_participants.get(&account_id).unwrap_or_else(|| Vector::new(format!("p#{}", account_id).as_bytes()));
+    events.push(&event_id);
+    self.events_by_participants.insert(&account_id, &events);
+  }
+
+  pub fn claim(&mut self, event_id: EventId) {
+    let event = self.get_event(&event_id).expect("Event does not exist");
+
+    if env::block_timestamp() >= event.deadline {
+      if event.cur_participants.len() < event.min_num {
+        let promise = event.cur_participants.iter().fold(None, |p: Option<Promise>, acc| {
+          if let Some(p) = p {
+            Some(p.and(Promise::new(acc).transfer(event.price)))
+          } else {
+            Some(Promise::new(acc).transfer(event.price))
+          }
+        });
+      } else {
+        Promise::new(event.beneficiary).transfer(event.price * (event.cur_participants.len() as u128 ));
+      }
+    }
+
+  }
+
+  pub fn get_all_events(&self) -> Vec<EventId> {
+    self.event_ids.to_vec()
+  }
+
+  pub fn get_events_by_owner(&self, account_id: AccountId) -> Vec<EventId> {
+    self.events_by_owner.get(&account_id).map(|v| v.to_vec()).unwrap_or_default()
+  }
+
+  pub fn get_events_by_participants(&self, account_id: AccountId) -> Vec<EventId> {
+    self.events_by_participants.get(&account_id).map(|v| v.to_vec()).unwrap_or_default()
+  }
+
+  pub fn get_event(&self, event_id: &EventId) -> Option<Event> {
+    self.events.get(&event_id)
   }
 }
